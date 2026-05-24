21c846329a7157e6294f1b9060191920e93a935e
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2026-05-24 22:09:00 +0530

    gitsign

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -16,7 +16,7 @@ require('lazy').setup({
   -- modular approach: using `require 'path.name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  -- require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.gitsigns',
 
   require 'kickstart.plugins.trouble',
 
